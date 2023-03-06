614df1cdd56273ca058e05d0c0367cda6bba73a3
Manuel <mdizenhaus@itba.edu.ar>
2023-03-06 20:05:37 -0300

    some more progress

--- TP1/TP1/src/CIM.java
+++ TP1/TP1/src/CIM.java
@@ -94,23 +94,115 @@ public class CIM {
     |____|____|____|
      */
 
-    private void calculateNeighbours(List<Particle> particles, List<List<List<Particle>>> particleMatrix, boolean round,
-                                     int cellX, int cellY) {
+    private void calculateNeighbours(List<Particle> particles, List<List<List<Particle>>> particleMatrix,
+                                     boolean round, int cellX, int cellY, int r_c) {
         //List<Particle> sameCell = particleMatrix.get(cellX).get(cellY).subList(particleMatrix.get(cellX).get(cellY).indexOf(p), particleMatrix.get(cellX).get(cellY).size()-1);
         for(Particle p : particles) {
-            for(int i=particles.indexOf(p); i<particles.size()-1; i++) {
+            for (int i = particles.indexOf(p) + 1; i < particles.size(); i++) {
                 /*
-
+                Condición de vecino:
                 raiz((x1 - x2)² + (y1-y2)^2) < r_c + 2r
-
                  */
-                if(/*cumplo condición para ser vecino*/true)
+                if (
+                        Math.pow(p.getX() - particles.get(i).getX(), 2) +
+                                Math.pow(p.getY() - particles.get(i).getY(), 2)
+                                < (2 * p.getR()) + r_c)
                     p.addNeighbour(particles.get(i));
             }
+            // Una a la derecha. Casos:
+            /*
+                1) pos X + 1 es MENOR al límite --> Da igual si es round o no, devuelvo ese
+                2) pos X + 1 es IGUAL al limite Y deja redondo --> Devuelvo 0
+                3) pos X + 1 es IGUAL al limite Y NO deja reodndo --> Devuelvo -1, indicando
+                    que no necesita análisis
+             */
+            int rightAdjacentCellX = cellX+1 < CIM_CELLS_PER_SIDE? cellX+1 : (
+                        round && cellX+1 == CIM_CELLS_PER_SIDE? 0 : -1
+                    );
+            /*
+            |   |   |///|
+            |   | * |///|
+            |___|___|///|
+            |   |   |   |
+            |   |   |   |
+            |___|___|___|
+             */
+            if(rightAdjacentCellX >= 0 && particleMatrix.get(rightAdjacentCellX).get(cellY) != null) {
+                for (Particle k : particleMatrix.get(rightAdjacentCellX).get(cellY)) {
+                    if (
+                            Math.pow(p.getX() - k.getX(), 2) +
+                                    Math.pow(p.getY() - k.getY(), 2)
+                                    < (2 * p.getR()) + r_c)
+                        p.addNeighbour(k);
+                }
+            }
+            int bottomAdjacentCellY = cellY+1 < CIM_CELLS_PER_SIDE? cellY+1 : (
+                    round && cellY+1 == CIM_CELLS_PER_SIDE? 0 : -1);
+            /*
+            |   |   |   |
+            |   | * |   |
+            |___|___|___|
+            |   |   |///|
+            |   |   |///|
+            |___|___|///|
+             */
+            if(rightAdjacentCellX >= 0 && bottomAdjacentCellY >= 0
+                    && particleMatrix.get(rightAdjacentCellX).get(bottomAdjacentCellY) != null){
+                for (Particle k : particleMatrix.get(rightAdjacentCellX).get(bottomAdjacentCellY)) {
+                    if (
+                            Math.pow(p.getX() - k.getX(), 2) +
+                                    Math.pow(p.getY() - k.getY(), 2)
+                                    < (2 * p.getR()) + r_c)
+                        p.addNeighbour(k);
+                }
+            }
+
+            /*
+            |   |   |   |
+            |   | * |   |
+            |___|___|___|
+            |   |///|   |
+            |   |///|   |
+            |___|///|___|
+             */
+
+            if(bottomAdjacentCellY >= 0  && particleMatrix.get(cellX).get(bottomAdjacentCellY) != null){
+                for (Particle k : particleMatrix.get(cellX).get(bottomAdjacentCellY)) {
+                    if (
+                            Math.pow(p.getX() - k.getX(), 2) +
+                                    Math.pow(p.getY() - k.getY(), 2)
+                                    < (2 * p.getR()) + r_c)
+                        p.addNeighbour(k);
+                }
+            }
+
+            /*
+            |   |   |   |
+            |   | * |   |
+            |___|___|___|
+            |///|   |   |
+            |///|   |   |
+            |///|___|___|
+             */
+            int leftAdjacentCellX = cellX - 1 >= 0 ? cellX - 1 : (
+                    round && cellX - 1 < 0? CIM_CELLS_PER_SIDE - 1: -1
+                    );
+            if(leftAdjacentCellX >= 0 && bottomAdjacentCellY >= 0
+                    && particleMatrix.get(leftAdjacentCellX).get(bottomAdjacentCellY) != null){
+                for (Particle k : particleMatrix.get(rightAdjacentCellX).get(bottomAdjacentCellY)) {
+                    if (
+                            Math.pow(p.getX() - k.getX(), 2) +
+                                    Math.pow(p.getY() - k.getY(), 2)
+                                    < (2 * p.getR()) + r_c)
+                        p.addNeighbour(k);
+                }
+            }
+
         }
+
     }
 
-    private void CellIndexMethod(int N, int r, int L, int M, int r_c){
+    private void CellIndexMethod(int N, int r, int L, int M, int r_c, boolean round){
 
         if(L/M <= r_c){
             throw new RuntimeException("L/M debe ser mayor exclusivo a r_c. Terminando ejecución");
@@ -131,20 +223,16 @@ public class CIM {
             for(int j=0; j<M; j++) {
                 List<Particle> auxList = particleMatrix.get(i).get(j);
                 if(auxList != null && !auxList.isEmpty()) {
+                    //TODO: Esto está dos veces. ¿Se podrá sacar?
                     List<Particle> particlesToEvaluate = particleMatrix.get(i).get(j);
-                    particlesToEvaluate.sort(new Comparator<Particle>() {
-                        @Override
-                        public int compare(Particle o1, Particle o2) {
+                    if(particlesToEvaluate != null) {
+                        particlesToEvaluate.sort((o1, o2) -> {
                             double distX = o1.getX() - o2.getX();
                             double retVal = distX != 0 ? distX : (o1.getY() - o2.getY());
-                            if (retVal > 0) {
-                                return 1;
-                            } else {
-                                return -1;
-                            }
-                        }
-                    });
-                    calculateNeighbours(particlesToEvaluate, particleMatrix, false, i, j);
+                            return retVal > 0 ? 1 : (retVal < 0 ? -1 : 0);
+                        });
+                    }
+                    calculateNeighbours(particlesToEvaluate, particleMatrix, round, i, j, r_c);
                 }
             }
         }
@@ -154,9 +242,14 @@ public class CIM {
 
 
     public static void main(String[] args) {
-        int L = CIM_LENGTH_SIDE;
-
-
+        boolean round = false;
+        int N = 1;
+        int L = 1;
+        int r = 1;
+        int M = 1;
+        int r_c = 1;
+        CIM cim = new CIM();
+        cim.CellIndexMethod(N, r, L, M, r_c, round);
     }
 
 }
