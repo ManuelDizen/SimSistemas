b82b2da103c015f3df7bd7514bef4e2278018d42
desimonef <67933006+desimonef@users.noreply.github.com>
2023-03-10 13:00:19 -0300

    Now generating both input and results files

--- TP1/TP1/src/Generator.java
+++ TP1/TP1/src/Generator.java
@@ -6,13 +6,30 @@ import java.util.List;
 
 public class Generator {
 
-    public static void generateOutputFile(List<Particle> particles, int N, double r_c, int M){
-        try(FileWriter fw = new FileWriter("Vecinos-" + "N:" + N + "-" +
-                "rc:" + r_c + "-M:" + M + ".txt", false);
+    public static void generateInputFile(int N, int i) {
+        try(FileWriter fw = new FileWriter("Dynamic" + N + "." + i + ".txt", false);
             BufferedWriter bw = new BufferedWriter(fw);
             PrintWriter out = new PrintWriter(bw))
         {
+            out.println(0);
+            for(int j=0; j<N; j++) {
+                StringBuilder aux = new StringBuilder();
+                aux.append(Math.random()* CIM.CIM_LENGTH_SIDE).append("\t").append(Math.random()*CIM.CIM_LENGTH_SIDE);
+                out.println(aux.toString());
+            }
+            out.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
+    public static void generateOutputFile(List<Particle> particles, int N, int file, double r_c, int M){
+        try(FileWriter fw = new FileWriter("Vecinos-" + "M_" + M + "-" + "N_" + N + "-" +
+                "rc_" + r_c + "." + file + ".txt", false);
+            BufferedWriter bw = new BufferedWriter(fw);
+            PrintWriter out = new PrintWriter(bw))
+        {
+            out.println("Cell Index Method: ");
             for(Particle p : particles) {
                 List<Particle> neighbours = p.getNeighbours();
                 StringBuilder aux = new StringBuilder();
@@ -22,6 +39,44 @@ public class Generator {
                 }
                 out.println(p.getIdx() + aux.toString());
             }
+            out.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void generateResults(Map<Integer, List<Long>> cellTime, Map<Integer, List<Long>> bruteTime) {
+        try(FileWriter fw = new FileWriter("Resultados.txt", false);
+            BufferedWriter bw = new BufferedWriter(fw);
+            PrintWriter out = new PrintWriter(bw))
+        {
+            out.println("Espacio de lado L=20, radio de interacción r_c=1, radio de partícula r=0.25");
+            out.println("Averiguamos el tiempo promedio (de 5 pasadas con input distinto) en microsegundos\npara distintas cantidades de partículas y distintas distribuciones de grilla,\npara ambos métodos");
+
+            for(int i=10; i<=30; i+=10) {
+
+                List<Long> cellList = cellTime.get(i);
+                List<Long> bruteList = bruteTime.get(i);
+
+                out.println("\nCantidad de partículas: " + i);
+
+                out.println("\n\t\t\tCell Index Method\t-\tBrute Force Method: ");
+
+                for(int j=1; j<=cellList.size(); j++) {
+                    out.println("\nGrilla " + j*2 + "x" + j*2 + ":\t\t" + cellList.get(j-1) + "\t\t\t\t\t\t\t" + bruteList.get(j-1));
+                }
+
+            }
+
+//            for(Particle p : particles) {
+//                List<Particle> neighbours = p.getNeighbours();
+//                StringBuilder aux = new StringBuilder();
+//                for(Particle a : neighbours) {
+//                    String s = Integer.toString(a.getIdx());
+//                    aux.append(",").append(s);
+//                }
+//                out.println(p.getIdx() + aux.toString());
+//            }
             out.close();
         } catch (IOException e) {
             e.printStackTrace();
