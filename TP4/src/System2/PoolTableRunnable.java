2c8addaf329292d43b893e80e4c756326ef58219
desimonef <fdesimone@itba.edu.ar>
2023-05-12 14:00:08 -0300

    (no branch): Auto stash before revert of "Try changing walls behaviour"
    Ahora sí, anda todo

--- TP4/src/System2/PoolTableRunnable.java
+++ TP4/src/System2/PoolTableRunnable.java
@@ -33,11 +33,20 @@ public class PoolTableRunnable {
         while(elapsed_time < total_time){
             for(int i = 0; i < table.particles.size(); i++){
                 Particle p = table.particles.get(i);
-                //System.out.println("Particle " + p.getIdx() + ": " + p.getX() + ", " + p.getY() + " vel: " + p.getVx() + ", " + p.getVy() + " acc: " + p.getDerivsX()[2] + ", " + p.getDerivsY()[2]);
+                //lo primero que hago es chequear si hay choque con alguna pared
+                if((p.getX() >= (table.LONG_SIDE-p.getRadius()) && p.getVx() >= 0) || (p.getX() <= p.getRadius()) && p.getVx() < 0){
+                    System.out.println("vertical wall ");
+                    p.applyBounceWithVerticalWall();
+                }
+                else if(p.getY() >= table.SHORT_SIDE || p.getY() <= 0){
+                    System.out.println("horizontal wall ");
+                    p.applyBounceWithHorizontalWall();
+                }
                 boolean flag = false;
                 Double[] forceP;
                 /*para cada partícula p, itero por el resto de las partículas a ver cuáles la están "influenciando"
                 acumulo las fuerzas para sacar la fuerza neta*/
+                //System.out.println("Particle " + p.getIdx() + ": " + p.getX() + ", " + p.getY() + " vel: " + p.getVx() + ", " + p.getVy() + " acc: " + p.getDerivsX()[2] + ", " + p.getDerivsY()[2]);
                 for(int j = i + 1; j < table.particles.size(); j++) {
                     Particle q = table.particles.get(j);
                     if (p.getNorm(p.getX(), p.getY(), q.getX(), q.getY()) < (p.getRadius() + q.getRadius())) {
@@ -52,20 +61,9 @@ public class PoolTableRunnable {
                 if(flag || p.hasAccumForce()) { //si al salir del for hubo al menos una colisión, entonces hay fuerza neta
                     p.applyUpdate(p.getForce());
                     p.resetForce(); //vuelvo la fuerza a 0 para empezar a acumular de 0 en la próxima iteración
-                }
-                else { //si no hubo choque con otra bocha, entro acá
-                    if(p.getX() >= table.LONG_SIDE || p.getX() <= 0){
-                        System.out.println("vertical wall ");
-                        p.applyBounceWithVerticalWall();
-                    }
-                    else if(p.getY() >= table.SHORT_SIDE || p.getY() <= 0){
-                        System.out.println("horizontal wall ");
-                        p.applyBounceWithHorizontalWall();
-                    }
-                    else {
-                        // al no haber colisión, hago update con fuerza 0
-                        p.applyUpdate(new Double[]{0.0, 0.0});
-                    }
+                } else {
+                    // al no haber colisión, hago update con fuerza 0
+                    p.applyUpdate(new Double[]{0.0, 0.0});
                 }
             }
 
