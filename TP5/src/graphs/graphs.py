be8672445f7d65484599200ea4a89057de639727
desimonef <fdesimone@itba.edu.ar>
2023-06-07 11:37:24 -0300

    Python script for graphs

--- TP5/src/graphs/graphs.py
+++ TP5/src/graphs/graphs.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+
 colors = ['red', 'blue', 'green', 'orange', 'cyan',
           'purple', 'darkgreen', 'darkkhaki', 'coral', 'red']
 
@@ -38,12 +39,17 @@ def calculate_average(arrays):
     min_length = min(array_lengths)
 
     average_array = []
+    std_array = []
     for i in range(min_length):
-        sum_values = sum(array[i] for array in arrays)
-        average = sum_values / len(arrays)
+        values = []
+        for array in arrays:
+            values += [array[i]]
+        average = np.mean(values)
+        std = np.std(values)
         average_array.append(average)
+        std_array.append(std)
 
-    return average_array
+    return average_array, std_array
 
 def caudal_medio(caudal):
     total = 0
@@ -51,55 +57,7 @@ def caudal_medio(caudal):
         total += i
     return total/len(caudal)
 
-# def caudal_graph(N, d):
-#     t_times = []
-#     t_particles = []
-#     times_to_leave = []
-#
-#     for i in range(0, 5):
-#         file_name = '../../../punto_a_iter_{}_{}.txt'.format(N, i)
-#         file = open(file_name, 'r')
-#         lines = file.read().split('\n')
-#         times = []
-#         particles = []
-#         for line in lines:
-#             data = line.split(" ")
-#             if data[0] == '' or float(data[0]) > 80.0:
-#                 continue
-#             times.append(float(data[0]))
-#             particles.append(float(data[1]))
-#         ttl = time_to_leave(times, particles)
-#         print(len(ttl))
-#         t_times.append(times)
-#         t_particles.append(particles)
-#         times_to_leave.append(ttl)
-
-    # fig = plt.figure()
-    # # for i in range(0,len(t_times)):
-    # #    plt.plot(t_times[i], t_particles[i], color = colors[i])
-    # array = (len(array) for array in t_times)
-    # print(array)
-    #
-    # avg = calculate_average(times_to_leave)
-    # yvalues = range(1, N)
-    # plt.plot(avg, yvalues, color='blue')
-    # plt.title("N = " + str(N))
-    # plt.xlabel("Tiempo (s)")
-    # plt.ylabel("Peatones evacuados")
-    # plt.grid(visible=True)
-    # plt.show()
-    #
-    # xprime, yprime = deriv(avg, yvalues)
-    #
-    # plt.plot(xprime, yprime, color='red')
-    # plt.title("N = " + str(N))
-    # plt.xlabel("Tiempo (s)")
-    # plt.ylabel("Caudal")
-    # plt.grid(visible=True)
-    # plt.show()
-    # return np.mean(yprime[5:65]), np.std(yprime[5:65])/(pow(60, 0.5))
-
-def caudal_N(N):
+def getdata(N):
     t_times = []
     t_particles = []
     times_to_leave = []
@@ -121,53 +79,62 @@ def caudal_N(N):
         t_particles.append(particles)
         times_to_leave.append(ttl)
 
-    fig = plt.figure()
     # for i in range(0,len(t_times)):
     #    plt.plot(t_times[i], t_particles[i], color = colors[i])
 
-    avg = calculate_average(times_to_leave)
-    yvalues = range(1, N)
+    avg, tt_std = calculate_average(times_to_leave)
+    return avg, tt_std
+
+def descarga(N):
+
+    avg, tt_std = getdata(N)
+
+    yrange = [*range(1, N)]
+    yarr = list(yrange)
+    yvalues = yarr[::-1]
+
     plt.plot(avg, yvalues, color='blue')
-    plt.title("N = " + str(N))
+    #plt.title("N = " + str(N))
     plt.xlabel("Tiempo (s)")
-    plt.ylabel("Peatones evacuados")
+    plt.ylabel("Peatones en recinto")
+    plt.errorbar(avg, yvalues, yerr=tt_std, fmt="+", elinewidth=1, capsize=5)
     plt.grid(visible=True)
     plt.show()
 
-    # avg_particles = calculate_average(t_particles)
-    #
-    # array_lengths = [len(t_t) for t_t in t_times]
-    # min_length = min(array_lengths)
-    #
-    # time_arr = t_times[0][:min_length]
-    #
-    # window = 200
-    # init = 200
-    #
-    # caudal_window = []
-    # times_mid = []
-    #
-    # print(min_length)
-    #
-    # while(init < min_length):
-    #     t2 = init-window
-    #     caudal = avg_particles[t2] - avg_particles[init]
-    #     t_m = time_arr[t2] + (time_arr[init] - time_arr[t2])/2
-    #     times_mid += [t_m]
-    #     caudal_window += [caudal/5]
-    #     init += 40
+def descarga_all():
 
+    Ns = [200, 260, 320, 380]
+
+    graph, lbl = plt.subplots()
+
+    for i in range(len(Ns)):
+        avg, tt_std = getdata(Ns[i])
+
+        yrange = [*range(1, Ns[i])]
+        yarr = list(yrange)
+        yvalues = yarr[::-1]
+
+        plt.plot(avg, yvalues, color=colors[i], label=f'N={Ns[i]}')
+        plt.errorbar(avg, yvalues, yerr=tt_std, fmt="+", elinewidth=1, capsize=5)
+    plt.xlabel("Tiempo (s)")
+    plt.ylabel("Peatones en recinto")
+    box = lbl.get_position()
+    lbl.set_position([box.x0, box.y0, box.width, box.height * 0.9])
+    plt.legend(loc='upper center', bbox_to_anchor=(0.5, 1.2), ncol=4)
+    plt.grid(visible=True)
+    plt.show()
+
+
+
+def calc_window(avg):
     times_mid = []
     caudal_window = []
 
     init = 5
     window = 5
 
-    print(max(avg))
-
-
     while init < max(avg):
-        start = init-window
+        start = init - window
         i = 0
         caudal = 0
         while i < len(avg):
@@ -177,11 +144,38 @@ def caudal_N(N):
             else:
                 break
             i += 1
-        times_mid += [start + (init-start)/2]
-        caudal_window += [caudal/window]
+        times_mid += [start + (init - start) / 2]
+        caudal_window += [caudal / window]
         init += 1
 
-    print(caudal_window)
+    return times_mid, caudal_window
+
+def caudal_all():
+    Ns = [200, 260, 320, 380]
+
+    graph, lbl = plt.subplots()
+
+    for i in range(len(Ns)):
+        avg, tt_std = getdata(Ns[i])
+
+        times_mid, caudal_window = calc_window(avg)
+
+        plt.scatter(times_mid, caudal_window, label=f'N={Ns[i]}')
+    plt.xlabel("Tiempo (s)")
+    plt.ylabel("Caudal")
+    box = lbl.get_position()
+    lbl.set_position([box.x0, box.y0, box.width, box.height * 0.9])
+    plt.legend(loc='upper center', bbox_to_anchor=(0.5, 1.2), ncol=4)
+    plt.grid(visible=True)
+    plt.show()
+
+
+def caudal_N(N):
+
+    avg, tt_std = getdata(N)
+
+    times_mid, caudal_window = calc_window(avg)
+
     plt.scatter(times_mid, caudal_window)
     plt.title("N = " + str(N))
     plt.xlabel("Tiempo (s)")
@@ -190,6 +184,17 @@ def caudal_N(N):
     plt.show()
     return np.mean(caudal_window[8:48]), np.std(caudal_window[8:48])
 
+def caudal_stationary(N):
+    avg, tt_std = getdata(N)
+
+    times_mid, caudal_window = calc_window(avg)
+
+    return np.mean(caudal_window[8:48]), np.std(caudal_window[8:48])
+
+
+descarga_all()
+caudal_all()
+
 ds = [1.2, 1.8, 2.4, 3.0]
 Ns = [200, 260, 320, 380]
 
@@ -197,12 +202,13 @@ caudal_med = []
 caudal_std = []
 
 for N in Ns:
-    med, std = caudal_N(N)
+    med, std = caudal_stationary(N)
     caudal_med += [med]
     caudal_std += [std]
 
-print(caudal_med)
-print(caudal_std)
+func = np.polyfit(ds, caudal_med, 1)
+
+print(func[0])
 
 plt.plot(ds, caudal_med)
 plt.errorbar(ds, caudal_med, yerr=caudal_std, fmt="o")
